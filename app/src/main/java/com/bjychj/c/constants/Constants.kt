7ab0f9e1ac17a92228bf3e8ab409360279b97c5a
yanjingtp <yanjingtp@gmail.com>
2019-04-04 08:29:35 +0800

    update baseUrl

--- app/src/main/java/com/bjychj/c/constants/Constants.kt
+++ app/src/main/java/com/bjychj/c/constants/Constants.kt
@@ -2,7 +2,7 @@ package com.bjychj.c.constants
 
 class Constants {
     companion object {
-        const val BaseUrl = "http://c.bjychj.com"
+        const val BaseUrl = "http://140.143.18.116:8080/"
         const val ReturnSuccess = ""
     }
 }
